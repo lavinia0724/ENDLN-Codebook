171ccc7a9c37ed89a5f0da01acadb584f389f310
CHA <76138565+WilliamChHuAn@users.noreply.github.com>
2021-09-20 12:54:46 +0800

    update

---
+++ Contents/Graph/Disjoint_Set.cpp
@@ -0,0 +1,32 @@
+int Find(int x) {
+    if (x == p[x])
+        return x;
+    return p[x] = find(p[x]);
+}
+
+void Union(int a, int b){
+
+    a = Find(a);
+    b = Find(b);
+
+    if (a == b)
+        return;
+    if (p[a] < p[b]){
+        p[a] = p[a] + p[b];
+    }
+    p[b] = a;
+}
+
+int main(){
+
+  // initial
+  for(i = 0; i < N; i++){
+      parents[i] = -1;
+  }
+
+  // input and union
+  for(i = 0; i < M; i++){
+      cin >> a >> b;
+      uni(a, b, parents);
+  }
+}
