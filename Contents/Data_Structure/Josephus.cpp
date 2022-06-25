7209b0226c4a2705c7636449e5dc9af1924cc05d
LAVI_0724 <83853006+LAVI724@users.noreply.github.com>
2022-06-26 01:08:12 +0800

    upload

--- Contents/Data_Structure/Josephus.cpp
+++ Contents/Data_Structure/Josephus.cpp
@@ -1,6 +1,6 @@
-int josephus ( int n , int k) {
+int josephus (int n, int k) {
     // 有 n 個人圍成一圈，每 k 個一次
-    return n > 1 ? ( josephus ( n - 1 , k ) + k ) % n : 0;
+    return n > 1 ? (josephus(n-1 , k) + k) % n : 0;
 }
 // 回傳最後一人的編號
 , 0 index
