63d74c32e47dc26be8786ecf81b94ff63add7a3e
LAVI_0724 <83853006+LAVI724@users.noreply.github.com>
2022-07-09 00:01:44 +0800

    upload

---
+++ Contents/Graph/Hungarian_algorithm.cpp
@@ -0,0 +1,59 @@
+const int maxn = 500+5;
+int t, N, bn, gn, match[maxn];
+bool visited[maxn];
+vector<vector<int>> G(maxn);
+struct People{
+    int h;
+    string music, sport;
+    People(){}
+    People(int h, string music, string sport){
+        this->h = h;
+        this->music = music;
+        this->sport = sport;
+    }
+}lef[maxn], rig[maxn];
+bool check(People boy, People girl){
+    if(abs(boy.h - girl.h) <= 40 && boy.music == girl.music && boy.sport != girl.sport) return true;
+    return false;
+}
+bool dfs(int s){
+    for(int i = 0; i < G[s].size(); ++i){
+        int v = G[s][i];
+        if(visited[v]) continue;
+        visited[v] = true;
+        if(match[v] == -1 || dfs(match[v])){
+            match[v] = s;
+            return true;
+        }
+    }
+    return false;
+}
+int Hungarian(){
+    int cnt = 0;
+    memset(match, -1, sizeof(match));
+    for(int i = 0; i < bn; ++i){
+        memset(visited, false, sizeof(visited));
+        if(dfs(i)) cnt++;
+    }
+    return cnt;
+}
+int main(){
+    cin >> t;
+    while(t--){
+        cin >> N;
+        bn = 0, gn = 0;
+        for(int i = 0; i <= N; ++i) G[i].clear();
+        int h;
+        string sex, music, sport;
+        for(int i = 0; i < N; ++i){
+            cin >> h >> sex >> music >> sport;
+            if(sex == "M") lef[bn++] = People(h, music, sport);
+            else rig[gn++] = People(h, music, sport);
+        }
+        for(int i = 0; i < bn; ++i){
+            for(int j = 0; j < gn; ++j)
+                if(check(lef[i], rig[j])) G[i].emplace_back(j);
+        }
+        cout << N - Hungarian() << endl;
+    }
+}
