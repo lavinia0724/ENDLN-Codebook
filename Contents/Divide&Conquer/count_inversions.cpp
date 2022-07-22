7066677cf14493e2e344f73947e8463bcf113112
LAVI_0724 <83853006+LAVI724@users.noreply.github.com>
2022-07-22 21:52:44 +0800

    upload

---
+++ Contents/Divide&Conquer/count_inversions.cpp
@@ -0,0 +1,21 @@
+int arr[maxn], buf[maxn];
+int count_inversions(int lef, int rig){ // 逆序數對
+    if(rig - lef <= 1) return 0;
+    int mid = (lef + rig)/2;
+    int ans = count_inversions(lef, mid) + count_inversions(mid, rig);
+    int i = lef, j = mid, k = lef;
+    while(i < mid || j < rig){
+        if(i >= mid) buf[k] = arr[j++];
+        else if(j >= rig) buf[k] = arr[i++];
+        else{
+            if(arr[i] <= arr[j]) buf[k] = arr[i++];
+            else{
+                buf[k] = arr[j++];
+                ans += mid - i;
+            }
+        }
+        k++;
+    }
+    for(int k = lef; k < rig; ++k) arr[k] = buf[k];
+    return ans;
+}
