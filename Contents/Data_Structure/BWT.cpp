b7aa411e34f45e744f1b1c02036cec042c3b0cc6
LAVI_0724 <83853006+LAVI724@users.noreply.github.com>
2022-09-29 10:30:22 +0800

    upload

---
+++ Contents/Data_Structure/BWT.cpp
@@ -0,0 +1,15 @@
+/*BWT 資料轉換演算法*/
+void BWT(){ 
+    for(int i = 0; i < n; ++i){
+        if(back[i] == 0)
+            mini[zero++] = i;
+    for(int i = 0; i < n; ++i)
+        if(back[i] == 1)
+            mini[zero++] = i;
+    int ptr = mini[0];
+    for(int i = 0; i < n; ++i){
+        cout << back[ptr] << " ";
+        ptr = mini[ptr];
+    }
+    cout << endl;
+}
